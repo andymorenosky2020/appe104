5530152c20eea8e775d5bb0ca0025e7c6fe3cdf8
andymorenosky2020 <117535077+andymorenosky2020@users.noreply.github.com>
2022-11-17 08:39:14 -0600

    Add files via upload

---
+++ routes/alumnos.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+//conexión con la base de datos
+const {connection} = require("../config.db");
+const getTodos = (request, response) => {
+    console.log("en el get")
+    connection.query("SELECT * FROM alumnos", 
+    (error, results) => {
+        if(error){
+            response.json({"SUCCESS":"no"})
+        }else{
+            response.json({"SUCCESS":"ok"})
+        }
+    });
+};
+
+//ruta
+app.route("/todos")
+.get(getTodos);
+
+
+const postAlumno = (request, response) => {
+    const {matricula, nombre, correo, telefono} = request.body;
+    connection.query("INSERT INTO alumnos(matricula, nombre, correo,telefono) VALUES (?,?,?,?) ", 
+    [matricula, nombre, correo,telefono],
+    (error, results) => {
+        if(error){
+            response.json({"resultado":"no"})
+        }else{
+            response.json({"resultado":"ok"})
+        }
+           
+        
+    });
+};
+
+//ruta
+app.route("/alumno")
+.post(postAlumno);
+
+
+const delAlumno = (request, response) => {
+    const id = request.params.id;
+    connection.query("Delete from alumnos where matricula = ?", 
+    [id],
+    (error, results) => {
+        if(error){
+            response.json({"resultado":"no"})
+        }else{
+            response.json({"resultado":"ok"})
+        }
+    });
+};
+
+//ruta
+app.route("/alumno/:id")
+.delete(delAlumno);
+
+
+module.exports = app;
